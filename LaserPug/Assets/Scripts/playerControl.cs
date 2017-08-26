f8e3e61d57cff765ecf0c99e9dcebcf15df66402
Koymaro <maro_1992@hotmail.es>
2017-08-26 18:14:28 +0200

    Create animation player and add movement to the player

---
+++ LaserPug/Assets/Scripts/playerControl.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControl : MonoBehaviour
+{
+    public float speed;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float x = Input.GetAxisRaw("Horizontal");//the value will be -1, 0, or 1 (for left, no input and right)
+        float y = Input.GetAxisRaw("Vertical");//the value will be -1, 0 or 1 (for down, no input and up)
+
+        //now based on the input we compute a direction vector and we normalize it to get a unit vector
+        Vector2 direction = new Vector2(x, y).normalized;
+
+        //now we call the function that computes and sets the player's position
+        Move(direction);
+
+	}
+
+    void Move(Vector2 direction)
+    {
+        //Find the screen limits to the player's movement (left, right, top and bottom edges of the screen)
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0)); //this is the bottom-left point (corner) of the screen
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1)); //this is the top-right point (corner) of the screen
+
+        max.x = max.x - 0.225f; //subtract the player sprite half width
+        min.x = min.x + 0.225f; //add the player sprite half width
+
+        max.y = max.y - 0.285f; //subtract the player sprite half height
+        min.y = min.y + 0.285f; //add the player sprite half height
+
+        //Get the player's current position
+        Vector2 playerPos = transform.position;
+
+        //Calculate the new position
+        playerPos += direction * speed * Time.deltaTime;
+
+        //Make sure the new position is not ourtisde the screen
+        playerPos.x = Mathf.Clamp(playerPos.x, min.x, max.x);
+        playerPos.y = Mathf.Clamp(playerPos.y, min.y, max.y);
+
+        //Update the player's postion
+        transform.position = playerPos;
+    }
+}
